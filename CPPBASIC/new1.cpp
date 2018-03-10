ff78aa222418252a0c5a495c6da69e46825085e3
mckim5ap <mckim5ap@gmail.com>
2018-03-10 14:31:53 -0500

    update 1

--- CPPBASIC/new1.cpp
+++ CPPBASIC/new1.cpp
@@ -7,24 +7,20 @@
 
 // std3.cpp
 
-//#include <stdio.h>
+#include <stdio.h>
 #include <cstdio>
-/*
-// cstdio 의 원리
-#include <stdio.h> // printf가 전역에 있다.
 
-namespace std
-{
-	using ::printf;
-}
-*/
-//---------------------------
+using namespace std;
+
 void foo(int a, int b, int c);
 int main()
 {
-	printf("hello\n");
+	/*printf("hello\n");
 	std::printf("hello\n");
-	foo(1,2,3);
+	foo(1,2,3);*/
+  const int c = 10;
+  int* p = (int*)&c;
+  cout << c << endl;
 }
 void foo (int a, int b, int c) {
 	std::printf("hello\n");
